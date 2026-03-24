efb4930bf08744dc974970c781375502b26eb3da
Bencrex <klee.benjamin6@gmail.com>
2026-03-24 22:11:55 +0000

    gestion-usuario.html loads partidas favoritos from db.json

--- src/pages/pagina-gestion-usuario/gestion-usuario.js
+++ src/pages/pagina-gestion-usuario/gestion-usuario.js
@@ -1,6 +1,5 @@
-const userID = document.getElementById("userID");
-const email = document.getElementById("email");
-
+const userID = document.getElementById('userID');
+const email = document.getElementById('email');
 
 async function initUsuario() {
   const user = JSON.parse(localStorage.getItem('user'));
@@ -28,16 +27,57 @@ async function fetchPartidosFavoritos(user) {
     const res = await fetch(
       `http://localhost:3000/partidas-favoritas?userID=${encodeURIComponent(user.id)}`,
     );
-    const data = await res.json();
+    const partidosFavoritosOfUser = await res.json();
 
-    if (data.length > 0) {
-      addPartidosFavoritosToPage(data);
+    if (partidosFavoritosOfUser.length > 0) {
+      fetchPartidosFavoritosFromDeporteAndAddToPage(partidosFavoritosOfUser);
     }
   } catch (err) {
     console.error(err);
   }
 }
 
-function addPartidosFavoritosToPage(partidosFavoritos) {
-  console.log(partidosFavoritos);
+async function fetchPartidosFavoritosFromDeporteAndAddToPage(
+  partidosFavoritosOfUser,
+) {
+  for (const partidoFavorito of partidosFavoritosOfUser) {
+    try {
+      // Run all fetches in parallel
+      const res = await fetch(`http://localhost:3000/soccer`);
+
+      // Parse all responses in parallel
+      const allData = await res.json();
+      const filteredData = allData.filter(
+        (item) => String(item.idEvent) === String(partidoFavorito.idEvent),
+      );
+      console.log(filteredData);
+      if (filteredData.length > 0) {
+        addPartidosFavoritosToPage(filteredData);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
 }
+
+function addPartidosFavoritosToPage(data) {
+  const { strEvent, dateEvent, strVideo } = data[0];
+
+  const match_list = document.getElementById('match-list');
+
+  match_list.innerHTML += `
+    <a class="match-item" href="${strVideo}" target="_blank">
+      <span>${strEvent}</span>
+      <span class="date">${formatDate(dateEvent)}</span>
+    </a>
+  `;
+}
+
+function formatDate(dateStr) {
+  return new Date(dateStr).toLocaleDateString('es-ES', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+}
+
